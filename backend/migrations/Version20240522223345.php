b93ac69612e599cf40bb051ed63b221b53234826
Mateusz Flis <flismati.s@gmail.com>
2024-05-23 00:35:11 +0200

    Deleting historical migrations and roll up the schema

--- backend/migrations/Version20240522223345.php
+++ backend/migrations/Version20240522223345.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240504230046 extends AbstractMigration
+final class Version20240522223345 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -25,15 +25,29 @@ final class Version20240504230046 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
-        $this->addSql('CREATE TABLE announcement_report (id INT AUTO_INCREMENT NOT NULL, given_at DATETIME NOT NULL, is_accepted TINYINT(1) DEFAULT NULL, details LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, user_id INT NOT NULL, announcement_id INT NOT NULL, INDEX IDX_DCFAC1DF913AEA17 (announcement_id), INDEX IDX_DCFAC1DFA76ED395 (user_id), UNIQUE INDEX UNIQ_DCFAC1DFA76ED395913AEA17 (user_id, announcement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE announcement_report ADD CONSTRAINT FK_DCFAC1DF913AEA17 FOREIGN KEY (announcement_id) REFERENCES announcement (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE announcement_report ADD CONSTRAINT FK_DCFAC1DFA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE TABLE announcement_upload (id INT AUTO_INCREMENT NOT NULL, media_object_id INT NOT NULL, announcement_id INT NOT NULL, UNIQUE INDEX UNIQ_F68504464DE5A5 (media_object_id), INDEX IDX_F685044913AEA17 (announcement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE announcement_upload ADD CONSTRAINT FK_F68504464DE5A5 FOREIGN KEY (media_object_id) REFERENCES media_object (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE announcement_upload ADD CONSTRAINT FK_F685044913AEA17 FOREIGN KEY (announcement_id) REFERENCES announcement (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
         $this->addSql('CREATE TABLE announcement_detail (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, locality VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, price INT DEFAULT NULL, description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, age INT DEFAULT NULL, gender VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, avatar_url VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, kind VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, age_type VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
+        );
+
+        $this->addSql('CREATE TABLE animal_feature (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, UNIQUE INDEX UNIQ_90081A5C5E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
+        );
+
+        $this->addSql('CREATE TABLE announcement_like (id INT AUTO_INCREMENT NOT NULL, given_at DATETIME NOT NULL, user_id INT NOT NULL, announcement_id INT NOT NULL, INDEX IDX_805248EFA76ED395 (user_id), UNIQUE INDEX UNIQ_805248EFA76ED395913AEA17 (user_id, announcement_id), INDEX IDX_805248EF913AEA17 (announcement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE announcement_like ADD CONSTRAINT FK_805248EF913AEA17 FOREIGN KEY (announcement_id) REFERENCES announcement (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE announcement_like ADD CONSTRAINT FK_805248EFA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
@@ -46,35 +60,17 @@ final class Version20240504230046 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
-        $this->addSql('CREATE TABLE announcement_like (id INT AUTO_INCREMENT NOT NULL, given_at DATETIME NOT NULL, user_id INT NOT NULL, announcement_id INT NOT NULL, INDEX IDX_805248EF913AEA17 (announcement_id), INDEX IDX_805248EFA76ED395 (user_id), UNIQUE INDEX UNIQ_805248EFA76ED395913AEA17 (user_id, announcement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE announcement_like ADD CONSTRAINT FK_805248EF913AEA17 FOREIGN KEY (announcement_id) REFERENCES announcement (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE announcement_like ADD CONSTRAINT FK_805248EFA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE TABLE announcement_report (id INT AUTO_INCREMENT NOT NULL, given_at DATETIME NOT NULL, is_accepted TINYINT(1) DEFAULT NULL, details LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, user_id INT NOT NULL, announcement_id INT NOT NULL, UNIQUE INDEX UNIQ_DCFAC1DFA76ED395913AEA17 (user_id, announcement_id), INDEX IDX_DCFAC1DFA76ED395 (user_id), INDEX IDX_DCFAC1DF913AEA17 (announcement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE announcement_report ADD CONSTRAINT FK_DCFAC1DF913AEA17 FOREIGN KEY (announcement_id) REFERENCES announcement (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE announcement_report ADD CONSTRAINT FK_DCFAC1DFA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
-        $this->addSql('CREATE TABLE announcement_media_object (announcement_id INT NOT NULL, media_object_id INT NOT NULL, INDEX IDX_EEA58FD1913AEA17 (announcement_id), INDEX IDX_EEA58FD164DE5A5 (media_object_id), PRIMARY KEY(announcement_id, media_object_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE announcement_media_object ADD CONSTRAINT FK_EEA58FD164DE5A5 FOREIGN KEY (media_object_id) REFERENCES media_object (id) ON UPDATE NO ACTION ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE announcement_media_object ADD CONSTRAINT FK_EEA58FD1913AEA17 FOREIGN KEY (announcement_id) REFERENCES announcement (id) ON UPDATE NO ACTION ON DELETE CASCADE');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-        );
-
-        $this->addSql('CREATE TABLE announcement_animal_feature (id INT AUTO_INCREMENT NOT NULL, is_positive TINYINT(1) DEFAULT NULL, feature_id INT NOT NULL, announcement_detail_id INT NOT NULL, INDEX IDX_C982DE89FC1FED97 (announcement_detail_id), INDEX IDX_C982DE8960E4B879 (feature_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE announcement_animal_feature (id INT AUTO_INCREMENT NOT NULL, is_positive TINYINT(1) DEFAULT NULL, feature_id INT NOT NULL, announcement_detail_id INT NOT NULL, INDEX IDX_C982DE8960E4B879 (feature_id), INDEX IDX_C982DE89FC1FED97 (announcement_detail_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE announcement_animal_feature ADD CONSTRAINT FK_C982DE8960E4B879 FOREIGN KEY (feature_id) REFERENCES animal_feature (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->addSql('ALTER TABLE announcement_animal_feature ADD CONSTRAINT FK_C982DE89FC1FED97 FOREIGN KEY (announcement_detail_id) REFERENCES announcement_detail (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-        );
-
-        $this->addSql('CREATE TABLE announcement (id INT AUTO_INCREMENT NOT NULL, is_accepted TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, animal_type_id INT NOT NULL, user_id INT NOT NULL, announcement_detail_id INT NOT NULL, deletion_detail_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_4DB9D91CB7EEA6C1 (deletion_detail_id), UNIQUE INDEX UNIQ_4DB9D91CFC1FED97 (announcement_detail_id), INDEX IDX_4DB9D91C4A93E3A9 (animal_type_id), INDEX IDX_4DB9D91CA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE announcement ADD CONSTRAINT FK_4DB9D91C4A93E3A9 FOREIGN KEY (animal_type_id) REFERENCES animal_type (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE announcement ADD CONSTRAINT FK_4DB9D91CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE announcement ADD CONSTRAINT FK_4DB9D91CB7EEA6C1 FOREIGN KEY (deletion_detail_id) REFERENCES announcement_deletion_detail (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE announcement ADD CONSTRAINT FK_4DB9D91CFC1FED97 FOREIGN KEY (announcement_detail_id) REFERENCES announcement_detail (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
@@ -86,7 +82,17 @@ final class Version20240504230046 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, roles JSON NOT NULL, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, phone VARCHAR(15) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, surname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, avatar_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D64986383B10 (avatar_id), UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE announcement (id INT AUTO_INCREMENT NOT NULL, is_accepted TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, animal_type_id INT NOT NULL, user_id INT NOT NULL, announcement_detail_id INT NOT NULL, deletion_detail_id INT DEFAULT NULL, INDEX IDX_4DB9D91C4A93E3A9 (animal_type_id), INDEX IDX_4DB9D91CA76ED395 (user_id), UNIQUE INDEX UNIQ_4DB9D91CB7EEA6C1 (deletion_detail_id), UNIQUE INDEX UNIQ_4DB9D91CFC1FED97 (announcement_detail_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE announcement ADD CONSTRAINT FK_4DB9D91C4A93E3A9 FOREIGN KEY (animal_type_id) REFERENCES animal_type (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE announcement ADD CONSTRAINT FK_4DB9D91CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE announcement ADD CONSTRAINT FK_4DB9D91CB7EEA6C1 FOREIGN KEY (deletion_detail_id) REFERENCES announcement_deletion_detail (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE announcement ADD CONSTRAINT FK_4DB9D91CFC1FED97 FOREIGN KEY (announcement_detail_id) REFERENCES announcement_detail (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
+        );
+
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, roles JSON NOT NULL, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, phone VARCHAR(15) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, surname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, avatar_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), UNIQUE INDEX UNIQ_8D93D64986383B10 (avatar_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D64986383B10 FOREIGN KEY (avatar_id) REFERENCES media_object (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
@@ -94,12 +100,6 @@ final class Version20240504230046 extends AbstractMigration
         );
 
         $this->addSql('CREATE TABLE animal_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, UNIQUE INDEX UNIQ_CBF3161D5E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-        );
-
-        $this->addSql('CREATE TABLE animal_feature (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, UNIQUE INDEX UNIQ_90081A5C5E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
     }
 
     public function down(Schema $schema): void
@@ -110,7 +110,7 @@ final class Version20240504230046 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
-        $this->addSql('DROP TABLE announcement_report');
+        $this->addSql('DROP TABLE announcement_upload');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
@@ -122,7 +122,7 @@ final class Version20240504230046 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
-        $this->addSql('DROP TABLE announcement_deletion_detail');
+        $this->addSql('DROP TABLE animal_feature');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
@@ -134,7 +134,13 @@ final class Version20240504230046 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
-        $this->addSql('DROP TABLE announcement_media_object');
+        $this->addSql('DROP TABLE announcement_deletion_detail');
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
+        );
+
+        $this->addSql('DROP TABLE announcement_report');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
@@ -146,13 +152,13 @@ final class Version20240504230046 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
-        $this->addSql('DROP TABLE announcement');
+        $this->addSql('DROP TABLE media_object');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
-        $this->addSql('DROP TABLE media_object');
+        $this->addSql('DROP TABLE announcement');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
@@ -165,11 +171,5 @@ final class Version20240504230046 extends AbstractMigration
         );
 
         $this->addSql('DROP TABLE animal_type');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
-        );
-
-        $this->addSql('DROP TABLE animal_feature');
     }
 }
